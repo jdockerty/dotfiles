b860a018893069ec13ff86d820f51644f191396a
Jack Dockerty <jdockerty19@gmail.com>
2022-12-17 12:47:10 +0000

    update remaps

--- private_dot_config/nvim/lua/config/remap.lua
+++ private_dot_config/nvim/lua/config/remap.lua
@@ -3,40 +3,40 @@ local nnoremap = require("config.keymap").nnoremap
 local vnoremap = require("config.keymap").vnoremap
 local nmap = require("config.keymap").nmap
 
-nnoremap("<leader>pv", "<cmd>Ex<cr>")
+vim.keymap.set("n","<leader>pv", "<cmd>Ex<cr>")
 
-nnoremap("<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input('Search for: ')})<CR>")
+vim.keymap.set("n","<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input('Search for: ')})<CR>")
 
 -- Telescope mappings
-nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+vim.keymap.set("n","<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 
-nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-nnoremap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-nnoremap("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+vim.keymap.set("n","<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+vim.keymap.set("n","<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+vim.keymap.set("n","<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
 
 -- Short for filename
-nnoremap("<leader>fn", vim.api.nvim_buf_get_name(0))
+vim.keymap.set("n","<leader>fn", vim.api.nvim_buf_get_name(0))
 
 -- Reload file easily, useful when using git and files are changing
-nnoremap("<leader>r", ":checktime<cr>")
+vim.keymap.set("n","<leader>r", ":checktime<cr>")
 
 -- Language Server Info, press 'i' to install in the shown list.
-nnoremap("<leader>li", ":LspInstallInfo<CR>")
+vim.keymap.set("n","<leader>li", ":LspInstallInfo<CR>")
 
 -- Save/Source current file, used when editing init.vim
-nnoremap("<silent><leader>so", ":w <CR> :source %<CR>")
+vim.keymap.set("n","<silent><leader>so", ":w <CR> :source %<CR>")
 
 -- Clear buffers, except the current one. The pipe must be escaped using '\'.
-nnoremap("<silent><leader>bd", ":%bd\\|e#<CR>")
+vim.keymap.set("n","<silent><leader>bd", ":%bd\\|e#<CR>")
 
-nnoremap("<leader>y", '"+y')
-vnoremap("<leader>y", '"+y')
-nmap("<leader>Y", '"+Y')
+vim.keymap.set("n","<leader>y", '"+y')
+vim.keymap.set("v","<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y"')
 
-nnoremap("<leader>d", '"_d')
-vnoremap("<leader>d", '"_d')
+vim.keymap.set("n","<leader>d", '"_d')
+vim.keymap.set("v","<leader>d", '"_d')
 
 
-nnoremap("<leader>ss", ":write <CR>")
+vim.keymap.set("n","<leader>ss", ":write <CR>")
 
-nnoremap("<S-k>", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n","<S-k>", "<cmd>lua vim.lsp.buf.hover()<CR>")
