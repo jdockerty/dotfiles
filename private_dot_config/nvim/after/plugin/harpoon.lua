bf3a4f95087aa9057f42228db5a00c4210dccd22
Jack Dockerty <jdockerty19@gmail.com>
2022-12-17 12:19:20 +0000

    add end to lua function call for harpoon

--- private_dot_config/nvim/after/plugin/harpoon.lua
+++ private_dot_config/nvim/after/plugin/harpoon.lua
@@ -4,8 +4,8 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<silent><leader>a", mark.add_file)
 vim.keymap.set("n", "<silent><leader>t", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<silent><leader>ay", function() ui.nav_file(1))
-vim.keymap.set("n", "<silent><leader>au", function() ui.nav_file(2))
-vim.keymap.set("n", "<silent><leader>ai", function() ui.nav_file(3))
-vim.keymap.set("n", "<silent><leader>ao", function() ui.nav_file(4))
+vim.keymap.set("n", "<leader>ay", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>au", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>ai", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>ao", function() ui.nav_file(4) end)
 
