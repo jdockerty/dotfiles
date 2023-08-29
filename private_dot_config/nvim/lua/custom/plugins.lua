5f30d8570c9bbff07055a41e779dd0bafd7704ba
Jack Dockerty <jdockerty19@gmail.com>
2023-08-29 21:23:45 +0100

    fix rust_analyzer lsp

--- private_dot_config/nvim/lua/custom/plugins.lua
+++ private_dot_config/nvim/lua/custom/plugins.lua
@@ -1,21 +1,25 @@
--- In order to modify the `lspconfig` configuration:
+local plugins = {
+
 {
   "neovim/nvim-lspconfig",
    config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
    end,
-} 
+},
 
 {
    "williamboman/mason.nvim",
    opts = {
       ensure_installed = {
         "lua-language-server",
-        "rust-analyzer",
+        "rust_analyzer",
         "gopls",
         "jsonnet-language-server"
 
       },
     },
   },
+}
+
+return plugins
