caf879351b95add11301b2a11d08d1930cdc8b5d
Jack Dockerty <jdockerty@influxdata.com>
2023-09-01 09:23:37 +0100

    add comment for format

--- private_dot_config/nvim/lua/custom/configs/null-ls.lua
+++ private_dot_config/nvim/lua/custom/configs/null-ls.lua
@@ -3,6 +3,8 @@ local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
+-- Formatting via LSP is defaulted to <leader>fm with null-ls.
+-- TODO: format on save
 local sources = {
 	formatting.prettier,
 	formatting.stylua,
